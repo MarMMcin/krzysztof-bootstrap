5c8dc758f357ba44be9495cc07a1cd82426b650f
MarMMcin <em-erce@wp.pl>
2019-09-24 15:33:44 +0200

    Updates

---
+++ precache-manifest.5681572ec084ac09bf6f886760240e59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5ec9d49bf9cb8c045b893e6f25059ff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f378164d22fb25217625",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "8bfc848baf71a816aee9",
+    "url": "/static/js/2.f65c3756.chunk.js"
+  },
+  {
+    "revision": "f378164d22fb25217625",
+    "url": "/static/js/main.408a2c05.chunk.js"
+  },
+  {
+    "revision": "0aca3f9ed8e709a37b28",
+    "url": "/static/js/runtime-main.5b8a2aae.js"
+  },
+  {
+    "revision": "eb54a4dd35168f7013b6026788425a7e",
+    "url": "/static/media/geodezja.eb54a4dd.jpg"
+  }
+]);
