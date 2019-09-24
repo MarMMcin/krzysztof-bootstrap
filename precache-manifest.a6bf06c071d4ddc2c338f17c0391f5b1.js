da34bc64f7b70f23e446d5b50a9b2e64ff0e7e5c
MarMMcin <em-erce@wp.pl>
2019-09-24 15:39:44 +0200

    Updates

---
+++ precache-manifest.a6bf06c071d4ddc2c338f17c0391f5b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c9aef7d0a38d2e519c4d64b8b085e88",
+    "url": "/krzysztof-bootstrap/index.html"
+  },
+  {
+    "revision": "f0c4db6a0c44228a0208",
+    "url": "/krzysztof-bootstrap/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "50523322ee7bf281d008",
+    "url": "/krzysztof-bootstrap/static/js/2.606bc416.chunk.js"
+  },
+  {
+    "revision": "f0c4db6a0c44228a0208",
+    "url": "/krzysztof-bootstrap/static/js/main.3c193d61.chunk.js"
+  },
+  {
+    "revision": "19edeee7d1de4cf64798",
+    "url": "/krzysztof-bootstrap/static/js/runtime-main.a585f619.js"
+  },
+  {
+    "revision": "eb54a4dd35168f7013b6026788425a7e",
+    "url": "/krzysztof-bootstrap/static/media/geodezja.eb54a4dd.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5681572ec084ac09bf6f886760240e59.js"
+  "/krzysztof-bootstrap/precache-manifest.a6bf06c071d4ddc2c338f17c0391f5b1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/krzysztof-bootstrap/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
